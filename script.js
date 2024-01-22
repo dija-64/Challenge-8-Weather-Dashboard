313f4c6be2618d14f98ed2f6439f03d8aafccddd
Dija <145768761+dija-64@users.noreply.github.com>
2024-01-22 08:49:08 +0000

    current weather & display weather functions

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-var APIKey = "95ded46496c80b8160db1b87c71937ca"
+var citySearch = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var cityTemp = $("#temperature");
+var cityHumid = $("#humidity");
+var cityWind = $("#wind-speed");
+var cityUV = $("#uv-index");
+var sCity = [];
+
+const APIKey = "95ded46496c80b8160db1b87c71937ca";
+const iconBaseURL = "https://openweathermap.org/img/wn/";
+
+function find(c) {
+    return sCity.findIndex(city => city.toUpperCase() === c.toUpperCase());
+}
+
+function displayWeather(event) {
+    event.preventDefault();
+    const city = citySearch.val().trim();
+    if (city !== "") {
+        currentWeather(city);
+    }
+}
+
+function currentWeather(city) {
+    const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKey}`;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+
+        const weathericon = response.weather[0].icon;
+        const iconurl = `${iconBaseURL}${weathericon}@2x.png`;
+
+        const date = new Date(response.dt * 1000).toLocaleDateString();
+
+        currentCity.html(`${response.name} (${date}) <img src="${iconurl}">`);
+
+        const tempF = ((response.main.temp - 273.15) * 1.80 + 32).toFixed(2);
+        cityTemp.html(`${tempF} &#8457`);
+
+        cityHumid.html(`${response.main.humidity}%`);
+
+        const windsmph = (response.wind.speed * 2.237).toFixed(1);
+        cityWind.html(`${windsmph} MPH`);
+
+        UVIndex(response.coord.lon, response.coord.lat);
+        forecast(response.id);
+
+        if (response.cod === 200) {
+            sCity = JSON.parse(localStorage.getItem("cityname")) || [];
+            console.log(sCity);
+            if (!sCity.includes(city.toUpperCase())) {
+                sCity.push(city.toUpperCase());
+                localStorage.setItem("cityname", JSON.stringify(sCity));
+                addToList(city);
+            }
+        }
+    });
+}
+
